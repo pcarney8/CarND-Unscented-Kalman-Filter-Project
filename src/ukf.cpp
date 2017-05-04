66ccf3379f8ba277c00375c86edc91f7a9b473be
Patrick C <patrick.s.carney@gmail.com>
2017-05-03 22:56:35 -0400

    lower

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -154,11 +154,11 @@ void UKF::Prediction(double delta_t) {
   MatrixXd P_aug_ = MatrixXd(n_aug_, n_aug_);
   P_aug_.fill(0);
   P_.fill(0);
-  P_ << 0.09, 0, 0, 0, 0,
-        0, 0.10, 0, 0, 0,
-        0, 0, 0.11, 0, 0,
-        0, 0, 0, 0.12, 0,
-        0, 0, 0, 0, 0.13;
+  P_ << 0.07, 0, 0, 0, 0,
+        0, 0.08, 0, 0, 0,
+        0, 0, 0.09, 0, 0,
+        0, 0, 0, 0.10, 0,
+        0, 0, 0, 0, 0.11;
 
     //create augmented mean state
     x_aug_ << x_,
